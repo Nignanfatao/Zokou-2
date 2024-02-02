eb9441ccf5401b7d6877175845ab2a7d96c7a505
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-02-02 16:24:48 +0000

    Update elfiche.js

--- bdd/elfiche.js
+++ bdd/elfiche.js
@@ -103,12 +103,12 @@ async function insertData() {
   }
 }
 // Fonction pour récupérer toutes les données
-async function getData(id) {
+async function getData(ide) {
   const client = await pool.connect();
 
   try {
    const query = 'SELECT e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22, e23, e24, e25, e26, e27, e28, e29, e30, e31, e32, e33, e34, e35 FROM elfiche WHERE id = $1';
-    const result = await client.query(query, [id]);
+    const result = await client.query(query, [ide]);
 
     return result.rows[0];
   } catch (error) {
